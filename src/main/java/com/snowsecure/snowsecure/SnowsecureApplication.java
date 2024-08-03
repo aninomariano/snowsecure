0b7ef0abe0826cb4cbd31ebee863d5f4c0e4a552
aninomariano <git config --global user.email>
2024-08-03 15:55:21 -0300

    init SB proyect
    Signed-off-by: aninomariano <git config --global user.email>

---
+++ src/main/java/com/snowsecure/snowsecure/SnowsecureApplication.java
@@ -0,0 +1,13 @@
+package com.snowsecure.snowsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowsecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowsecure/snowsecure/SnowsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowsecure.snowsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
